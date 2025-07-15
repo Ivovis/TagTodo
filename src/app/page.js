9690a29271ea61c11ccd9c42e4c0ec8690cd7607
Christopher Hunn <christopher.hunn@gmail.com>
2025-07-15 11:48:31 +0100

    feat: Clerk set up and working

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,21 @@
-import Image from "next/image";
+import Link from "next/link";
+import {
+  UserButton,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+} from "@clerk/nextjs";
+
+import { auth } from "@clerk/nextjs/server";
 
 export default function Home() {
-  return <>Home Page</>;
+  return (
+    <>
+      <SignedOut>
+        <h1>Please sign in to continue</h1>
+      </SignedOut>
+      <SignedIn>{/* show the list of tasks here */}</SignedIn>
+    </>
+  );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,27 @@
 import Link from "next/link";
 
+import {
+  UserButton,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+} from "@clerk/nextjs";
+
 export default async function Header() {
   return (
     <>
       <div>
         <Link href="/">Home</Link>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <div className="flex justify-between gap-3">
+            <SignInButton mode="modal" />
+            <SignUpButton mode="modal" />
+          </div>
+        </SignedOut>
         <Link href="/about">About</Link>
       </div>
     </>
